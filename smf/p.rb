3e90a1e7fc2cca9577fb09c29e99f2c8f748cbad
kani <sns@kabura-ya.net>
2019-04-20 18:36:22 +0900

    hex

--- smf/p.rb
+++ smf/p.rb
@@ -1,6 +1,10 @@
 while buf = gets
     buf.scan(/<li>(\d+),(\d+),(\d+),(\d+)<\/li>/){
         |m|
-        p m
+        time = $4.to_i
+        a = $1.to_i
+        b = $2.to_i
+        c = $3.to_i
+        put [a.to_s(16), b.to_s(16), c.to_s(16), time.to_s(16), '\n'.join(',')
     }
 end
