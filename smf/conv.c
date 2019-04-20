e0573980ed0ffbca838fb1012b30aec76f1d962c
kani <sns@kabura-ya.net>
2019-04-20 19:35:10 +0900

    fixed filename

--- smf/conv.c
+++ smf/conv.c
@@ -24,7 +24,7 @@ int main( int argc, char* argv[] )
   int size;
   int c1, c2, c3, c4, c5;
   char buf[256];
-  fp2 = fopen("sample.txt", "r");
+  fp2 = fopen("tmp.txt", "r");
   fp = fopen("out.mid", "w" );
   if ( fp == NULL || fp2 == NULL ){
     perror("file:" );
