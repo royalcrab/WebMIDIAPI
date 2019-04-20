5117de04ec6ae67a377b0c19c13a5c8c87b8d6d8
kani <sns@kabura-ya.net>
2019-04-20 19:29:33 +0900

    binarize

--- smf/p.rb
+++ smf/p.rb
@@ -1,20 +1,23 @@
 pre = 0
+open('tmp.txt', 'bw'){ |f|
+    while buf = gets
+        buf.scan(/<li>(\d+),(\d+),(\d+),(\d+)<\/li>/){
+            |m|
+            time = ($4.to_i - pre) if pre > 0
+            time = 1000 if pre == 0
+            time = 16000 if time > 16000
+            pre = $4.to_i
+            a = $1.to_i
+            b = $2.to_i
+            c = $3.to_i
 
-while buf = gets
-    buf.scan(/<li>(\d+),(\d+),(\d+),(\d+)<\/li>/){
-        |m|
-        time = ($4.to_i - pre) if pre > 0
-        time = 1000 if pre == 0
-        time = 16000 if time > 16000
-        pre = $4.to_i
-        a = $1.to_i
-        b = $2.to_i
-        c = $3.to_i
+            ta = time & 0x7f;
+            tb = (time >> 7) & 0x7f;
+            tb |= 0x80 if (time & 0x80) > 0
 
-        ta = time & 0x7f;
-        tb = (time >> 7) & 0x7f;
-        tb |= 0x80 if (time & 0x80) > 0
+            f.write([a,b,c,tb,ta].pack("C*"))
 
-        print [a, b, c, tb, ta, "\n"].join(',')
-    }
-end
+#            print [a, b, c, tb, ta, "\n"].join(',')
+        }
+    end
+}
